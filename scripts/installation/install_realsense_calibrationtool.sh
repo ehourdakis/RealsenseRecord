2f4288a3bba7c9f0622b2134e30ee9fc1aafaccd
Emmanouil Hourdakis <hourdakis.emmanouil@gmail.com>
2022-02-09 16:23:46 +0200

    Updated the DynamicCalibrator key

--- scripts/installation/install_realsense_calibrationtool.sh
+++ scripts/installation/install_realsense_calibrationtool.sh
@@ -1,12 +1,18 @@
+# Install required libraries
 sudo apt-get update
 sudo apt-get install libusb-dev libusb-1.0-0-dev
 sudo apt-get install libglfw3 libglfw3-dev
 sudo apt-get install freeglut3 freeglut3-dev
 
+# Add sources
 echo 'deb https://librealsense.intel.com/Debian/apt-repo bionic main' | sudo tee
 /etc/apt/sources.list.d/realsense-public.list
 
-sudo apt-key adv --keyserver keys.gnupg.net --recv-key 6F3EFCDE
+# The key below is incuded in the default realsense installation instructions, however some report erros with using.
+# sudo apt-key adv --keyserver keys.gnupg.net --recv-key 6F3EFCDE
+
+# Alexandra P. reported success with the key below
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C8B3A55A6F3EFCDE
 sudo apt-get update 
 sudo apt-get install librscalibrationtool
 
